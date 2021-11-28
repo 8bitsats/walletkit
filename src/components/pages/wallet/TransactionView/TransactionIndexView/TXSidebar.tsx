d8817f6fe46d027ce6a2faf985b62e33f917917a
Goki Rajesh <goki@goki.so>
2021-11-28 10:49:07 +0530

    wherever you like

--- src/components/pages/wallet/TransactionView/TransactionIndexView/TXSidebar.tsx
+++ src/components/pages/wallet/TransactionView/TransactionIndexView/TXSidebar.tsx
@@ -1,6 +1,8 @@
-import { FaCheckCircle, FaQuestionCircle } from "react-icons/fa";
+import copyToClipboard from "copy-to-clipboard";
+import { FaCheckCircle, FaLink, FaQuestionCircle } from "react-icons/fa";
 
 import { useSmartWallet } from "../../../../../hooks/useSmartWallet";
+import { notify } from "../../../../../utils/notifications";
 import { AddressLink } from "../../../../common/AddressLink";
 import { useTransaction } from "../context";
 
@@ -9,7 +11,22 @@ export const TXSidebar: React.FC = () => {
   const { tx, title, executedAt, eta } = useTransaction();
   return (
     <>
-      <div tw="text-xs font-semibold text-gray-500 border-b pb-2">{title}</div>
+      <div tw="text-xs border-b pb-2">
+        <div tw="flex">
+          <span tw="font-semibold text-secondary w-[90px]">{title}</span>
+          <div tw="text-secondary">
+            <FaLink
+              onClick={() => {
+                copyToClipboard(window.location.href);
+                notify({
+                  message: "Transaction link copied to clipboard.",
+                  description: "Paste it wherever you like.",
+                });
+              }}
+            />
+          </div>
+        </div>
+      </div>
       <div tw="text-xs mt-4">
         <div tw="flex mb-4">
           <span tw="text-secondary w-[90px]">Key</span>
