cc7a6dd66701812af4e99dd5bed66d42b8a7ab16
Goki Rajesh <goki@goki.so>
2021-12-01 14:56:51 +0530

    verbiage

--- src/components/pages/wallet/programs/WalletProgramsView/index.tsx
+++ src/components/pages/wallet/programs/WalletProgramsView/index.tsx
@@ -16,7 +16,7 @@ export const WalletProgramsView: React.FC = () => {
   return (
     <BasicPage
       title="Programs"
-      description="Manage the programs that are upgradeable by this wallet."
+      description="Manage the programs that this wallet can upgrade."
     >
       {programData.isLoading ? (
         <LoadingPage />
