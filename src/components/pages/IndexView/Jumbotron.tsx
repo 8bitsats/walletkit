5412c0683eb63b89546aebc63fa78fced25c45ab
Goki Rajesh <goki@goki.so>
2021-12-01 13:23:37 +0530

    for humans by humans

--- src/components/pages/IndexView/Jumbotron.tsx
+++ src/components/pages/IndexView/Jumbotron.tsx
@@ -1,21 +1,46 @@
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { css, theme } from "twin.macro";
+import Typewriter from "typewriter-effect";
 
 import { Button } from "../../common/Button";
 
 export const Jumbotron: React.FC = () => {
   return (
     <header tw="text-center w-full mx-auto flex flex-col items-center gap-6">
-      <div tw="max-w-2xl flex flex-col gap-6">
+      <div tw="max-w-2xl flex flex-col items-center gap-6">
         <h2 tw="text-primary uppercase font-semibold tracking-widest">
           For DAOs, teams, and secure individuals
         </h2>
-        <h1 tw="text-5xl font-black mb-2">Secure your admin keys.</h1>
-        <p tw="text-secondary font-semibold text-2xl">
-          Goki allows you to create, manage, and view
+        <h1 tw="text-5xl font-black leading-tight">
+          Solana multisig
           <br />
-          multisig wallets with transparency.
+          made for humans.
+        </h1>
+        <span tw="text-2xl font-black inline-flex">
+          <span>The safest way to&nbsp;</span>
+          <span tw="text-secondary">
+            <Typewriter
+              options={{
+                strings: [
+                  "manage your admin keys.",
+                  "run your DAO.",
+                  "store your company treasury.",
+                  "upgrade programs.",
+                  "grow your savings.",
+                  "audit a DAO.",
+                  "create a Timelock.",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
+        </span>
+        <p tw="text-secondary font-semibold text-xl">
+          Goki provides an interface to create, manage, and
+          <br />
+          audit multisig wallets on Solana.
         </p>
       </div>
       <Link to="/onboarding/new">
@@ -27,7 +52,7 @@ export const Jumbotron: React.FC = () => {
           <FaArrowRight />
         </Button>
       </Link>
-      <div tw="relative w-full mt-20">
+      <div tw="relative w-full mt-12">
         <div
           tw="absolute -left-24 top-0 -right-24 -mt-10 bottom-0 w-full"
           css={css`
