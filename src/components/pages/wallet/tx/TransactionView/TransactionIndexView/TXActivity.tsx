f952677a77ca8abc2c2c658877ead7b13d81a8f1
Goki Rajesh <goki@goki.so>
2021-12-01 14:24:30 +0530

    update key

--- src/components/pages/wallet/tx/TransactionView/TransactionIndexView/TXActivity.tsx
+++ src/components/pages/wallet/tx/TransactionView/TransactionIndexView/TXActivity.tsx
@@ -16,10 +16,10 @@ export const TXActivity: React.FC = () => {
       <h2 tw="mt-8 text-gray-800 font-semibold mb-4">Activity</h2>
       {txsWithEvents ? (
         <div tw="text-xs flex flex-col gap-4">
-          {txsWithEvents.map(({ tx, event }) => {
+          {txsWithEvents.map(({ tx, event }, i) => {
             return (
               <div
-                key={tx.sig}
+                key={`${tx.sig}_${i}`}
                 tw="text-gray-500 inline-flex items-center gap-4"
               >
                 <span>
