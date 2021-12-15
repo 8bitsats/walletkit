5c04219d78dbde461b8bee956594980c0c59e338
Goki Rajesh <goki@goki.so>
2021-12-16 01:07:30 +0530

    Proposals list empty state

--- src/components/pages/governance/GovernanceOverviewView/ProposalsList/index.tsx
+++ src/components/pages/governance/GovernanceOverviewView/ProposalsList/index.tsx
@@ -1,21 +1,41 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
+import { EmptyState } from "../../../../common/EmptyState";
+import { useGovernor } from "../../hooks/useGovernor";
 import { useProposals } from "../../hooks/useProposals";
 import { PageNav } from "./PageNav";
 import { PlaceholderCard } from "./PlaceholderCard";
 import { ProposalCard } from "./ProposalCard";
 
-const NUM_PLACER_HOLDERS = 10;
+const NUM_PLACEHOLDERS = 0;
 const MAX_PROPOSAL_COUNT = 4;
 
 export const ProposalsList: React.FC = () => {
+  const { path } = useGovernor();
   const proposals = useProposals();
   const [currentPage, setCurrentPage] = useState(0);
   const allProposals = [
     ...proposals,
-    ...new Array<null>(NUM_PLACER_HOLDERS).fill(null),
+    ...new Array<null>(NUM_PLACEHOLDERS).fill(null),
   ];
   const startCursor = currentPage * MAX_PROPOSAL_COUNT;
+
+  if (allProposals.length === 0) {
+    return (
+      <div>
+        <EmptyState title="There aren't any proposals yet.">
+          <Link
+            tw="text-primary hover:text-white transition-colors"
+            to={`${path}/proposals/create`}
+          >
+            Create a proposal
+          </Link>
+        </EmptyState>
+      </div>
+    );
+  }
+
   return (
     <>
       {allProposals
@@ -40,7 +60,7 @@ export const ProposalsList: React.FC = () => {
 };
 
 const calcPageTotal = (numProposals: number): number => {
-  const totalProposals = numProposals + NUM_PLACER_HOLDERS;
+  const totalProposals = numProposals + NUM_PLACEHOLDERS;
   const div = Math.floor(totalProposals / MAX_PROPOSAL_COUNT);
   return div + (totalProposals % MAX_PROPOSAL_COUNT ? 1 : 0);
 };
--- src/components/pages/governance/proposals/ProposalsListView/index.tsx
+++ src/components/pages/governance/proposals/ProposalsListView/index.tsx
@@ -1,12 +1,28 @@
+import { Link } from "react-router-dom";
+
+import { Button } from "../../../../common/Button";
 import { Card } from "../../../../common/governance/Card";
 import { GovernancePage } from "../../../../common/governance/GovernancePage";
 import { ProposalsList } from "../../GovernanceOverviewView/ProposalsList";
+import { useGovernor } from "../../hooks/useGovernor";
 import { LegendsNeverDie } from "./LegendsNeverDie";
 
 export const ProposalsListView: React.FC = () => {
+  const { path } = useGovernor();
   return (
     <GovernancePage title="Governance Proposals" right={<LegendsNeverDie />}>
-      <Card title="All Proposals">
+      <Card
+        title={
+          <div tw="flex w-full items-center justify-between">
+            <h2>All Proposals</h2>
+            <Link to={`${path}/proposals/create`}>
+              <Button tw="px-3" variant="primary">
+                Create Proposal
+              </Button>
+            </Link>
+          </div>
+        }
+      >
         <ProposalsList />
       </Card>
     </GovernancePage>
