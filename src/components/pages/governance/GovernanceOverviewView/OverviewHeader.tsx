8e3f3957a82bbebda454f55c3ad94c8a8e5517af
Goki Rajesh <goki@goki.so>
2021-12-16 13:42:11 +0530

    Fix animation

--- src/components/pages/governance/GovernanceOverviewView/OverviewHeader.tsx
+++ src/components/pages/governance/GovernanceOverviewView/OverviewHeader.tsx
@@ -31,7 +31,15 @@ export const OverviewHeader: React.FC = () => {
     <div tw="flex flex-wrap gap-2.5">
       <StatCard tw="flex-grow flex-basis[100%] md:flex-basis[auto]">
         <StatInner>
-          <span tw="text-white text-xl font-semibold">{lockedSupplyFmt}</span>
+          <div tw="h-7 flex items-center">
+            {lockedSupplyFmt ? (
+              <span tw="text-white text-xl font-semibold">
+                {lockedSupplyFmt}
+              </span>
+            ) : (
+              <div tw="flex animate-pulse bg-gray h-4 w-12 rounded" />
+            )}
+          </div>
           <span tw="text-xs font-semibold text-coolGray-300 tracking-tighter">
             {govToken?.symbol} Locked
           </span>
@@ -39,7 +47,15 @@ export const OverviewHeader: React.FC = () => {
       </StatCard>
       <StatCard tw="flex-grow md:(flex-basis[200px] flex-grow-0)">
         <StatInner>
-          <span tw="text-white text-xl font-semibold">{totalSupplyFmt}</span>
+          <div tw="h-7 flex items-center">
+            {totalSupplyFmt ? (
+              <span tw="text-white text-xl font-semibold">
+                {totalSupplyFmt}
+              </span>
+            ) : (
+              <div tw="flex animate-pulse bg-gray h-4 w-12 rounded" />
+            )}
+          </div>
           <span tw="text-xs font-semibold text-coolGray-300 tracking-tighter">
             Total Supply of {govToken?.symbol}
           </span>
