7d7f9341adb928b047873aea6ae62eb25a9bbc69
Goki Rajesh <goki@goki.so>
2021-12-17 05:45:27 +0530

    easier font color

--- src/components/pages/governance/locker/LockerIndexView/CardItem.tsx
+++ src/components/pages/governance/locker/LockerIndexView/CardItem.tsx
@@ -6,7 +6,7 @@ interface Props {
 export const CardItem: React.FC<Props> = ({ label, children }: Props) => {
   return (
     <div tw="px-7 py-4 border-b border-warmGray-800">
-      <span tw="text-warmGray-600 text-sm">{label}</span>
+      <span tw="text-warmGray-400 text-sm">{label}</span>
       <div tw="text-xl text-white mt-0.5">{children}</div>
     </div>
   );
