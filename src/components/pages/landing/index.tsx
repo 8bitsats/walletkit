3e53e3144b42c3b8af6ab876af62222eb98b887e
Goki Rajesh <goki@goki.so>
2021-12-20 08:12:48 +0530

    Fix goki page

--- src/components/pages/landing/index.tsx
+++ src/components/pages/landing/index.tsx
@@ -2,7 +2,6 @@ import { GokiIndexView } from "./GokiIndexView";
 import { TribecaIndexView } from "./TribecaIndexView";
 
 export const IndexView: React.FC = () => {
-  return <TribecaIndexView />;
   return (
     <>
       {process.env.REACT_APP_APP_CONFIG === "tribeca" ? (
