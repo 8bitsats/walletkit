f1a7c426b724f2585778863feb974acd77d1e048
michaelhly <michaelhly@gmail.com>
2021-12-20 14:30:41 -0600

    Revert "clean up"
    This reverts commit 4846b0dfd277e35b94e2b206cfe7da033f993c6e.

--- src/components/pages/governance/locker/LockerIndexView/EscrowInfo.tsx
+++ src/components/pages/governance/locker/LockerIndexView/EscrowInfo.tsx
@@ -80,7 +80,7 @@ export const EscrowInfo: React.FC<Props> = ({ className }: Props) => {
             <OuterButtonContainer escrowExists={!!escrow}>
               <Link to={`${path}/locker/lock`} tw="flex-grow">
                 <Button
-                  tw="border-primary dark:text-primary hover:dark:text-white"
+                  tw="w-full border-primary dark:text-primary hover:dark:text-white"
                   type="button"
                   size="md"
                   variant="outline"
@@ -90,10 +90,10 @@ export const EscrowInfo: React.FC<Props> = ({ className }: Props) => {
               </Link>
             </OuterButtonContainer>
             {escrow && (
-              <OuterButtonContainer escrowExists={true}>
+              <OuterButtonContainer escrowExists={!!escrow}>
                 <Link to={`${path}/locker/extend`} tw="flex-grow">
                   <Button
-                    tw="border-primary dark:text-primary hover:dark:text-white"
+                    tw="w-full border-primary dark:text-primary hover:dark:text-white"
                     type="button"
                     size="md"
                     variant="outline"
