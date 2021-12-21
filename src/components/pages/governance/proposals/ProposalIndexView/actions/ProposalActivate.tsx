bf0d4f3c4673be7aeaf65b99e3c8578c12ab3c98
Michael Huang <michaelhly@gmail.com>
2021-12-20 19:24:04 -0600

    Better styling for proposal activation buttons (#10)

--- src/components/pages/governance/proposals/ProposalIndexView/actions/ProposalActivate.tsx
+++ src/components/pages/governance/proposals/ProposalIndexView/actions/ProposalActivate.tsx
@@ -52,15 +52,80 @@ export const ProposalActivate: React.FC<Props> = ({
   return (
     <Card title="Actions">
       <div tw="px-7 py-4 text-sm">
+        {!earliestActivationTime || !governorData ? (
+          <LoadingPage />
+        ) : earliestActivationTime > new Date() ? (
+          <div tw="flex flex-col gap-2">
+            <p>
+              You must wait{" "}
+              {formatDurationSeconds(
+                governorData.accountInfo.data.params.votingDelay.toNumber()
+              )}{" "}
+              for this proposal to be activated.
+            </p>
+            <p>
+              The proposal may be activated at{" "}
+              {earliestActivationTime?.toLocaleString(undefined, {
+                timeZoneName: "short",
+              })}{" "}
+              by anyone who possesses at least{" "}
+              {minActivationThreshold?.formatUnits()}.
+            </p>
+          </div>
+        ) : minActivationThreshold &&
+          veBalance?.greaterThan(minActivationThreshold) ? (
+          <div tw="flex justify-center items-center">
+            <Button
+              disabled={!escrow}
+              tw="w-3/4 dark:text-white hover:dark:text-primary hover:dark:border-primary"
+              variant="outline"
+              onClick={async () => {
+                invariant(escrow);
+                const tx = escrow.escrowW.activateProposal(
+                  proposal.proposalKey
+                );
+                const { pending, success } = await handleTX(
+                  tx,
+                  "Activate Proposal"
+                );
+                if (!pending || !success) {
+                  return;
+                }
+                await pending.wait();
+                await sleep(1_000);
+                await refetch();
+                onActivate();
+              }}
+            >
+              Activate Proposal
+            </Button>
+          </div>
+        ) : (
+          <div tw="flex flex-col gap-2">
+            <p>
+              You must have at least{" "}
+              <strong>{minActivationThreshold?.formatUnits()}</strong> to
+              activate this proposal for voting.
+            </p>
+            {veBalance ? (
+              <p>You currently have {veBalance?.formatUnits()}.</p>
+            ) : (
+              <p>You currently don't have any tokens vote locked.</p>
+            )}
+            <Link tw="flex justify-center items-center" to={`${path}/locker`}>
+              <Button tw="w-3/4 mt-4">Lock Tokens</Button>
+            </Link>
+          </div>
+        )}
         <div tw="flex flex-col gap-2">
           {governorW &&
             proposal.proposalData.proposer.equals(
               governorW.provider.wallet.publicKey
             ) && (
-              <div>
+              <div tw="flex justify-center items-center">
                 <Button
-                  variant="danger"
-                  tw="dark:text-white"
+                  variant="outline"
+                  tw="w-3/4 mt-4 dark:text-white hover:dark:text-red-500 hover:dark:border-red-500"
                   onClick={async () => {
                     const tx = governorW.cancelProposal({
                       proposal: proposal.proposalKey,
@@ -76,70 +141,6 @@ export const ProposalActivate: React.FC<Props> = ({
                 </Button>
               </div>
             )}
-          <div>
-            {!earliestActivationTime || !governorData ? (
-              <LoadingPage />
-            ) : earliestActivationTime > new Date() ? (
-              <div tw="flex flex-col gap-2">
-                <p>
-                  You must wait{" "}
-                  {formatDurationSeconds(
-                    governorData.accountInfo.data.params.votingDelay.toNumber()
-                  )}{" "}
-                  for this proposal to be activated.
-                </p>
-                <p>
-                  The proposal may be activated at{" "}
-                  {earliestActivationTime?.toLocaleString(undefined, {
-                    timeZoneName: "short",
-                  })}{" "}
-                  by anyone who possesses at least{" "}
-                  {minActivationThreshold?.formatUnits()}.
-                </p>
-              </div>
-            ) : minActivationThreshold &&
-              veBalance?.greaterThan(minActivationThreshold) ? (
-              <Button
-                disabled={!escrow}
-                variant="primary"
-                onClick={async () => {
-                  invariant(escrow);
-                  const tx = escrow.escrowW.activateProposal(
-                    proposal.proposalKey
-                  );
-                  const { pending, success } = await handleTX(
-                    tx,
-                    "Activate Proposal"
-                  );
-                  if (!pending || !success) {
-                    return;
-                  }
-                  await pending.wait();
-                  await sleep(1_000);
-                  await refetch();
-                  onActivate();
-                }}
-              >
-                Activate Proposal
-              </Button>
-            ) : (
-              <div tw="flex flex-col gap-2">
-                <p>
-                  You must have at least{" "}
-                  <strong>{minActivationThreshold?.formatUnits()}</strong> to
-                  activate this proposal for voting.
-                </p>
-                {veBalance ? (
-                  <p>You currently have {veBalance?.formatUnits()}.</p>
-                ) : (
-                  <p>You currently don't have any tokens vote locked.</p>
-                )}
-                <Link to={`${path}/locker`} tw="mt-4">
-                  <Button>Lock Tokens</Button>
-                </Link>
-              </div>
-            )}
-          </div>
         </div>
       </div>
     </Card>
