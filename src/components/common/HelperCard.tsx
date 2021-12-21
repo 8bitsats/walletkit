977ea44a794d9b05ef5e09d2238a5b755fcce9b4
Goki Rajesh <goki@goki.so>
2021-12-21 09:37:42 +0530

    developers coming soon

--- src/components/common/HelperCard.tsx
+++ src/components/common/HelperCard.tsx
@@ -14,7 +14,8 @@ export const HelperCard: React.FC<Props> = ({
       css={[
         variant === "primary" && tw`border-primary bg-primary text-primary-100`,
         variant === "error" && tw`border-red-500 bg-red-500 text-red-100`,
-        tw`px-4 py-2 rounded border bg-opacity-20  text-sm`,
+        variant === "muted" && tw`border-slate-500 bg-slate-500 text-slate-200`,
+        tw`px-4 py-2 rounded border bg-opacity-20 text-sm`,
       ]}
     >
       {children}
--- src/components/pages/governance/proposals/ProposalCreateView/actions/RawTX.tsx
+++ src/components/pages/governance/proposals/ProposalCreateView/actions/RawTX.tsx
@@ -1,6 +1,7 @@
 import { extractErrorMessage } from "@saberhq/sail";
 import { Transaction } from "@solana/web3.js";
 
+import { HelperCard } from "../../../../../common/HelperCard";
 import { Textarea } from "../../../../../common/inputs/InputText";
 
 interface Props {
@@ -15,32 +16,46 @@ export const RawTX: React.FC<Props> = ({
   setTxRaw,
 }: Props) => {
   return (
-    <label tw="flex flex-col gap-1" htmlFor="instructionsRaw">
-      <span tw="text-sm">Transaction (base64)</span>
-      <Textarea
-        id="instructionsRaw"
-        tw="h-auto font-mono"
-        rows={4}
-        placeholder="Paste raw base64 encoded transaction message"
-        value={txRaw}
-        onChange={(e) => {
-          setTxRaw(e.target.value);
-          try {
-            const buffer = Buffer.from(e.target.value, "base64");
-            const tx = Transaction.from(buffer);
-            if (tx.instructions.length === 0) {
-              throw new Error("no instruction data");
+    <>
+      <HelperCard variant="muted">
+        <div tw="prose prose-sm prose-light">
+          <p>
+            This page allows proposing any arbitrary transaction for execution
+            by the DAO. The fee payer and recent blockhash will not be used.
+          </p>
+          <p>
+            This page is intended to be only be used by developers. Docs are
+            coming soon.
+          </p>
+        </div>
+      </HelperCard>
+      <label tw="flex flex-col gap-1" htmlFor="instructionsRaw">
+        <span tw="text-sm">Transaction (base64)</span>
+        <Textarea
+          id="instructionsRaw"
+          tw="h-auto font-mono"
+          rows={4}
+          placeholder="Paste raw base64 encoded transaction message"
+          value={txRaw}
+          onChange={(e) => {
+            setTxRaw(e.target.value);
+            try {
+              const buffer = Buffer.from(e.target.value, "base64");
+              const tx = Transaction.from(buffer);
+              if (tx.instructions.length === 0) {
+                throw new Error("no instruction data");
+              }
+              setError(null);
+            } catch (err) {
+              setError(
+                `Invalid transaction data: ${
+                  extractErrorMessage(err) ?? "(unknown)"
+                }`
+              );
             }
-            setError(null);
-          } catch (err) {
-            setError(
-              `Invalid transaction data: ${
-                extractErrorMessage(err) ?? "(unknown)"
-              }`
-            );
-          }
-        }}
-      />
-    </label>
+          }}
+        />
+      </label>
+    </>
   );
 };
