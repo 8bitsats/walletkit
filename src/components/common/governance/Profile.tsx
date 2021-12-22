2cc6954d96341b3d7b264ce23191dc5ac76fa4e0
michaelhly <michaelhly@gmail.com>
2021-12-22 17:19:27 -0600

    Use blockie for proposer profile

--- src/components/common/governance/Profile.tsx
+++ src/components/common/governance/Profile.tsx
@@ -1,4 +1,5 @@
 import type { PublicKey } from "@solana/web3.js";
+import makeBlockie from "ethereum-blockies-base64";
 
 import { displayAddress } from "../../../utils/programs";
 import { AddressLink } from "../AddressLink";
@@ -8,14 +9,17 @@ interface Props {
 }
 
 export const Profile: React.FC<Props> = ({ address }: Props) => {
+  const addressStr = address.toString();
   return (
     <div tw="bg-warmGray-850 p-3 text-sm rounded">
       <div tw="flex gap-2">
-        <div tw="h-10 w-10 rounded-full border border-dashed"></div>
+        <img
+          tw="h-10 w-10 rounded-full"
+          alt={`profile-${addressStr}`}
+          src={makeBlockie(addressStr)}
+        ></img>
         <div tw="flex flex-col">
-          <span tw="text-white font-medium">
-            {displayAddress(address.toString())}
-          </span>
+          <span tw="text-white font-medium">{displayAddress(addressStr)}</span>
           <AddressLink tw="text-gray-400" address={address} />
         </div>
       </div>
