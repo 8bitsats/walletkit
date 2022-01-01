a83b3e6633c50d3384756b887b839669dda3f3c1
michaelhly <michaelhly@gmail.com>
2022-01-01 01:30:34 -0800

    DAO Not Found -> Not Found

--- src/components/pages/governance/GovernanceNotFoundPage.tsx
+++ src/components/pages/governance/GovernanceNotFoundPage.tsx
@@ -14,7 +14,7 @@ export const GovernanceNotFoundPage: React.FC = () => {
           <div tw="flex flex-col gap-4 md:(gap-8 flex-row min-h-[120px]) flex-wrap items-center justify-between w-full">
             <div tw="flex flex-col self-start md:self-center">
               <h1 tw="text-2xl md:text-3xl font-bold text-white tracking-tighter">
-                DAO Not Found
+                Not Found
               </h1>
             </div>
           </div>
